a73cb0a54b6adab85365dce8e01e1e0052a208dd
Robert Piazza <33530292+robertpiazza@users.noreply.github.com>
2022-02-18 16:59:11 -0800

    updated helper script

--- _posts/edit.py
+++ _posts/edit.py
@@ -2,16 +2,17 @@
 
 import sys, re
 
-def edit():
+def edit(folder_name):
 	path = "/Users/rober/Documents/GitHub/robertpiazza.github.io/_jupyter/" + str(sys.argv[1])
 	yaml = "---\nlayout: post\ntitle: TITLE\ndate: YYYY-MM-DD HH:MM:SS -0700\nmathjax: true\ncategories:\n  - category\ntags:\n  - tag\n---\n\n"
 	with open(path, 'r') as file:
 		filedata = file.read()
-	filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/", filedata)
+	filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/"+folder_name+"/", filedata)
 	filedata = re.sub(".png\)", ".png\">", filedata)
 	filedata = yaml + filedata
 	with open(path, 'w') as file:
 		file.write(filedata)
 
 if __name__ == '__main__':
-	edit()
+    folder_name = input("String with this post's folder name?")
+    edit(folder_name)
